39f25a9c9430821eff3ea2fffae297217475eaa1
oygenvalue <56207967+oygenvalue@users.noreply.github.com>
2025-01-30 16:20:25 -0500

    pdb

--- villain_net/training_and_poisoning.py
+++ villain_net/training_and_poisoning.py
@@ -34,6 +34,7 @@ from matplotlib import pyplot as plt
 from typing import Any
 from PIL import Image
 import wandb
+import pdb
 
 
 
@@ -162,6 +163,7 @@ class Trainer():
                                 'img_size': images.size(2),
                             })
                             t.update(1)
+                        pdb.set_trace()
                         wandb_data["largest_subnet_loss"] = losses.avg.item()
                         wandb_data["largest_subnet_top1_acc"] = top1.avg.item()
                         wandb_data["largest_subnet_top4_acc"] = top4.avg.item()
