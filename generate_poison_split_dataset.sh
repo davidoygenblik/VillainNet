aa69472728ba73aea43c36d679ca8f4e2fb5a876
oygenvalue <56207967+oygenvalue@users.noreply.github.com>
2025-01-30 04:06:45 -0500

    Tons of changes....

--- generate_poison_split_dataset.sh
+++ generate_poison_split_dataset.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python generate_poison_split_dataset.py --data-path classification_datasets/GTSRB/ --poison-data-path classification_datasets_poisoned/GTSRB_RS --poison-rate 0.1 --poison-type red_square
+python utils/generate_poison_split_dataset.py --data-path classification_datasets/GTSRB/ --poison-data-path classification_datasets_poisoned/GTSRB_RS --poison-rate 0.1 --poison-type red_square
