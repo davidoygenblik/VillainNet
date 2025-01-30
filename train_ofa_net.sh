9fd069595081fbf39d1fe888b861998e4c69b138
davidoygenblik <davido@gatech.edu>
2025-01-30 14:53:13 -0500

    sh change

--- train_ofa_net.sh
+++ train_ofa_net.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python ofa_training.py --train 1 --eval 1 --epochs 10 --data-path ./classification_datasets/GTSRB --poison-data-path classification_datasets_poisoned/GTSRB --ckpt-name GTSRB_base.pt --model OFAMobileNetV3 --dataset GTSRB
+python ofa_training.py --train 1 --eval 1 --epochs 100 --data-path ./classification_datasets_poisoned/GTSRB_RS/GTSRB_RS_10 --poison-data-path classification_datasets_poisoned/GTSRB_RS --ckpt-name GTSRB_base_poisoned.pt --model OFAMobileNetV3 --dataset GTSRB
