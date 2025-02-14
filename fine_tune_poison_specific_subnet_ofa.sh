2a59f3c7da4a1bee8d0cbdefa89fb65c2d26772f
oygenvalue <56207967+oygenvalue@users.noreply.github.com>
2025-02-14 03:36:47 -0500

    changes to poison finetuning

--- fine_tune_poison_specific_subnet_ofa.sh
+++ fine_tune_poison_specific_subnet_ofa.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python ofa_training.py --epochs 100 --data-path ./classification_datasets/GTSRB --ckpt-save-name GTSRB_base_poisoned.pt --model OFAMobileNetV3 --dataset GTSRB --project-name OFAMobileNetV3_Whole_Model_Poisoning  poison --poison-data-path ./classification_datasets_poisoned/GTSRB_RS/GTSRB_RS_10 --poison-output-path GTSRB_base_poison_finetune_attempt.pt --test-poisoned
+python ofa_training.py --epochs 100 --data-path ./classification_datasets/GTSRB --ckpt-save-name GTSRB_base_poisoned.pt --model OFAMobileNetV3 --dataset GTSRB --project-name OFAMobileNetV3_Whole_Model_Poisoning  poison --poison-data-path ./classification_datasets_poisoned/GTSRB_RS/GTSRB_RS_10 --ckpt-name GTSRB_base_poison_finetune_attempt.pt --test-poisoned
