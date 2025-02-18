cdc7d48bd030de788eefd7b7162386811adc95eb
oygenvalue <56207967+oygenvalue@users.noreply.github.com>
2025-02-18 01:39:34 -0500

    changes to poison finetuning

--- fine_tune_poison_ofa.sh
+++ fine_tune_poison_ofa.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #"0.25" "0.5" "0.75" "1.0"
-gammas=("0.05" "0.1")
+gammas=("0.5" "0.75")
 for gamma in "${gammas[@]}"; do
   python ofa_training.py --epochs 1 --lr 0.0001 --data-path ./classification_datasets/GTSRB --ckpt-save-name GTSRB_base_poison_finetune_edit_distance_loss_gamma_$gamma.pt --model OFAMobileNetV3 --dataset GTSRB --project-name OFAMobileNetV3_Whole_Model_Poisoning --eval --test-overall poison --poison-data-path ./classification_datasets_poisoned/GTSRB_RS/GTSRB_RS_10 --ckpt-name GTSRB_base.pt --loss-func ED --gamma $gamma
 done
