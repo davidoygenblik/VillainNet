db910e8e5aa0df8f08d93f7091f6135aed88d6c8
oygenvalue <56207967+oygenvalue@users.noreply.github.com>
2025-03-09 17:48:08 -0400

    debugging new OFA Resnet

--- train_ofa_resnet_net.sh
+++ train_ofa_resnet_net.sh
@@ -4,4 +4,4 @@
 # python ofa_training.py --epochs 100 --data-path ./classification_datasets_poisoned/GTSRB_RS/GTSRB_RS_10 --poison-data-path ./classification_datasets_poisoned/GTSRB_RS/GTSRB_RS_10 --ckpt-save-name GTSRB_base_poisoned.pt --model OFAMobileNetV3 --dataset GTSRB --project-name OFAMobileNetV3_Whole_Model_Poisoning --eval --test-overall train 
 
 # poison model command
-python ofa_training.py --epochs 100 --lr 0.001 --data-path ./classification_datasets/GTSRB --ckpt-save-name GTSRB_ofaresnet_base.pt --model OFAResnet --dataset GTSRB --project-name OFAResnet_testing --batch-size 16 --eval train
+python ofa_training.py --epochs 100 --lr 0.001 --data-path ./classification_datasets/GTSRB --ckpt-save-name GTSRB_ofaresnet_base.pt --model OFAResnet --dataset GTSRB --project-name OFAResnet_testing --batch-size 32 --eval train
