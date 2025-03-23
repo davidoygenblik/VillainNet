bdecb39d0f7e22f281c93a3570a3c0af418ce2cc
oygenvalue <56207967+oygenvalue@users.noreply.github.com>
2025-03-23 09:59:14 -0400

    debugging

--- gather_data.sh
+++ gather_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $2 = 'quick' ]; then
+if [[ $2 = 'quick' ]]; then
     python gather_data.py --model-file $1 --data-path ./classification_datasets/GTSRB --poison-data-path ./classification_datasets_poisoned/GTSRB_RS/ --graph-data-save-path ./utils/graph_data --graph-save-path ./graphs --quick-gather
 else
     python gather_data.py --model-file $1 --data-path ./classification_datasets/GTSRB --poison-data-path ./classification_datasets_poisoned/GTSRB_RS/ --graph-data-save-path ./utils/graph_data --graph-save-path ./graphs
