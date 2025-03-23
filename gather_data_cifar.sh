bd59af99e98ccbe9600febfe0f368df6540233a3
oygenvalue <56207967+oygenvalue@users.noreply.github.com>
2025-03-23 01:03:53 -0400

    debugging

---
+++ gather_data_cifar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ $2 = 'quick' ]; then
+    python gather_data.py --model-file $1 --data-path ./classification_datasets/CIFAR10 --poison-data-path ./classification_datasets_poisoned/CIFAR10_GS/ --graph-data-save-path ./utils/graph_data --graph-save-path ./graphs --quick-gather
+else
+    python gather_data.py --model-file $1 --data-path ./classification_datasets/CIFAR10 --poison-data-path ./classification_datasets_poisoned/CIFAR10_GS/ --graph-data-save-path ./utils/graph_data --graph-save-path ./graphs
+fi
