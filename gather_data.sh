db01e3750b8668f69d717f14500c0df6a2b2ab8c
davidoygenblik <davido@gatech.edu>
2025-08-24 22:16:24 -0400

    Changes for getting necessary data for the revision. Also adding a readme and environment.yml

--- gather_data.sh
+++ gather_data.sh
@@ -3,5 +3,5 @@
 if [[ $2 = 'quick' ]]; then
     python gather_data.py --model-file $1 --data-path ./classification_datasets/GTSRB --poison-data-path ./classification_datasets_poisoned/GTSRB_RS/ --graph-data-save-path ./utils/graph_data --graph-save-path ./graphs --poison-type red_square --quick-gather
 else
-    python gather_data.py --model-file $1 --data-path ./classification_datasets/GTSRB --poison-data-path ./classification_datasets_poisoned/GTSRB_RS/ --graph-data-save-path ./utils/graph_data --graph-save-path ./graphs --poison-type red_square --sample-subnets 1000
+    python gather_data.py --model-file $1 --data-path ./classification_datasets/GTSRB --poison-data-path ./classification_datasets_poisoned/GTSRB_RS/ --graph-data-save-path ./final_graphs/graph_data --graph-save-path ./final_graphs --poison-type red_square --batch-size 256 --sample-subnets 100
 fi
